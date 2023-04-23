aead557fc2eadeeb85cd5fb3f6e27e43b7cf44e3
leexinfast <leexinfast@foxmail.com>
2023-04-23 16:22:50 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-row>
+      <el-button loading>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
   </div>
 </template>
 
